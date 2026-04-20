e71160c4b512ff664bad8e3ecbe87ad9f5fdb18d
Colin Park <colin.park@cimon.com>
2026-04-20 18:37:45 +0900

    Implement V4L2 camera streaming

---
+++ src/cameraview.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <QWidget>
+#include <QSize>
+#include <linux/videodev2.h>
+
+class QLabel;
+class QComboBox;
+class QPushButton;
+class QTimer;
+class QImage;
+class QBuffer;
+
+class CameraView : public QWidget {
+    Q_OBJECT
+public:
+    explicit CameraView(QWidget* parent = nullptr);
+    ~CameraView();
+
+private slots:
+    void refreshDevices();
+    void startCamera();
+    void stopCamera();
+    void pollFrame();
+
+private:
+    void setStatus(const QString& text);
+    bool openDevice(const QString& path);
+    void closeDevice();
+    bool initCapture();
+    bool readFrame(QImage& out);
+    void releaseBuffers();
+
+    QComboBox* m_deviceCombo = nullptr;
+    QPushButton* m_refreshBtn = nullptr;
+    QPushButton* m_startBtn = nullptr;
+    QPushButton* m_stopBtn = nullptr;
+    QLabel* m_imageLabel = nullptr;
+    QLabel* m_statusLabel = nullptr;
+    QTimer* m_timer = nullptr;
+    int m_fd = -1;
+    bool m_running = false;
+    QSize m_frameSize;
+    v4l2_pix_format m_pixfmt{};
+    static constexpr int kBufferCount = 4;
+    void* m_buffers[kBufferCount] = {nullptr, nullptr, nullptr, nullptr};
+    size_t m_bufferLengths[kBufferCount] = {0, 0, 0, 0};
+};
